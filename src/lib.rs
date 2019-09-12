b27f31e8d9eb6cdb965a953aef2a292420837bae
Chris Wininger <cwininger@sonatype.com>
2019-09-12 18:47:31 -0400

    shrug

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+extern crate js_sys;
+
 mod utils;
 
 use wasm_bindgen::prelude::*;
 use std::fmt;
+use js_sys::Math;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -94,7 +97,9 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+                let coin: f64 = Math::random();
+
+                if coin > 0.5 {
                     Cell::Alive
                 } else {
                     Cell::Dead
@@ -111,5 +116,45 @@ impl Universe {
 
     pub fn render(&self) -> String {
         self.to_string()
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height)
+            .map(|_i| Cell::Dead)
+            .collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height)
+            .map(|_i| Cell::Dead)
+            .collect();
+    }
+}
+
+// === Not Exposed to JS (TESTING ONLY ===
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
